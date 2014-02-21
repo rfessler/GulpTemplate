93ee9809337f9fc740aa23ebb8874116d54d082f
rfessler <rickfessler@gmail.com>
2014-02-21 14:41:45 -0500

    js-concat working

--- gulpfile.js
+++ gulpfile.js
@@ -28,12 +28,15 @@ var gulp = require('gulp'),
 /*******************************************************************************
 2. FILE DESTINATIONS (RELATIVE TO ASSSETS FOLDER)
 *******************************************************************************/
+var src_js_assets = 'src/assets/js',
+	src_scss_assets = 'src/assets/scss'
+
 var target = {
-	sassDirSrc : 'src/scss/**/*.scss',				// all sass files
+	sassDirSrc : 'src/assets/scss/**/*.scss',				// all sass files
 	cssDirDist : 'dist/assets/css',					// where to put minified css
 	cssDistFile : pkg.name + '.min.css',			// css output file name
 	cssComponentFilesSrc : [
-		'src/components/normalize-css/normalize.css'		
+		'src/assets/components/normalize-css/normalize.css'		
 	],
 	cssComponentDirDist : 'dist/assets/components',
 	jsLintFilesSrc : [
@@ -43,14 +46,18 @@ var target = {
 		'src/assets/js/euc.js',
 		'src/assets/js/ajax.js'
 	],							// all js that should be linted
-	jsUglifyFilesSrc :[],							// all js files that should not be concatinated
+	jsUglifyFilesSrc : [
+		'src/assets/components/jquery/jquery.js',
+		'src/assets/components/modernizr/modernizr.js'
+	],						// all js files that should not be concatinated
 	jsConcactFilesSrc : [],							// all js files that should be concatinated
 	jsSrcFileList : [],								// JS files to include
 	jsDistFile : pkg.name + '.min.js',				// compiled JS files
 	jsDirDist : 'dist/assets/js',					// where to put minified js
 	jsComponentFilesSrc : [
-		'src/components/modernizr/modernizr.js'		
+		'src/assets/components/modernizr/modernizr.js'		
 	],
+	jsConcatFileName : pkg.name + 'min.js',
 	jsComponentDirDist : 'dist/assets/components', 	// where to put componet js (minified) js
 	cleansingAreas : [
 		'dist/assets/js/*.js',
@@ -60,7 +67,7 @@ var target = {
 };
 
 /*******************************************************************************
-3. SASS TASK
+3. SASS TASK  -- working rmf 2/21
 *******************************************************************************/
 gulp.task('sass', function() {
 	gulp.src(['src/scss/kickoff.scss'])
@@ -73,41 +80,33 @@ gulp.task('sass', function() {
 });
 
 
-
-
-
-
-
-
-
 /*******************************************************************************
 4. JS TASKS
 *******************************************************************************/
-// lint custom js
+// lint custom js  -- working rmf 2/21
 gulp.task('js-lint', function() {
     gulp.src(target.jsLintFilesSrc)                     // get the files
         .pipe(jshint())                                 // lint the files
         .pipe(jshint.reporter(stylish))                 // present the results in a beautiful way
 });
 
-
-
-
 // minify all js files that should not be concatinated
+gulp.task('js-uglify', function() {
+    gulp.src(target.jsUglifyFilesSrc)                      // get the files
+        .pipe(uglify())                                 // uglify the files   	
+        .pipe(gulp.dest(target.jsDirDist))                // where to put the files
+});
+
 
 // minify & concatinate all other js
-
-gulp.task('js', function() {
-  gulp.src(scriptFiles)
-  	.pipe(concat(pkg.name + '.min.js'))
-  	.pipe(stripDebug())
-  	.pipe(uglify())
-  	.pipe(gulp.dest(jsDistDir));
-
-  gulp.src('src/components/**/*.js')
-  	.pipe(concat('vendor.js'))
-  	.pipe(uglify())  	
-  	.pipe(gulp.dest(jsComponentDistDir));
+gulp.task('js-concat', function() {
+  return gulp.src(target.jsLintFilesSrc)				// get the files
+  	.pipe(concat(pkg.name + '.js'))  					// concatinate to one file
+  	.pipe(stripDebug())									// strip debug statements
+  	.pipe(gulp.dest('dist/assets/js'))					// write the file non minified version
+  	.pipe(rename(pkg.name + '.min.js'))					// rename to minified version
+	.pipe(uglify())										// uglify the file
+  	.pipe(gulp.dest('dist/assets/js'));					// write the uglified version
 });
 
 
