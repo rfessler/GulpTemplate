6cdb334b637167ed4e3878cb322cff205ba8296f
rfessler <rickfessler@gmail.com>
2014-02-21 12:58:00 -0500

    jshint working

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,7 @@ var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
 	concat = require('gulp-concat'),
 	jshint = require('gulp-jshint'),
+	stylish = require('jshint-stylish'),                // make errors look good in shell
 	uglify = require('gulp-uglify'),
 	rename = require('gulp-rename'),
 	lr = require('tiny-lr'),
@@ -35,7 +36,13 @@ var target = {
 		'src/components/normalize-css/normalize.css'		
 	],
 	cssComponentDirDist : 'dist/assets/components',
-	jsLintFilesSrc : [],							// all js that should be linted
+	jsLintFilesSrc : [
+		'src/assets/js/global.js',
+		'src/assets/js/togglepaneloffers.js',
+		'src/assets/js/zipandoffers.js',
+		'src/assets/js/euc.js',
+		'src/assets/js/ajax.js'
+	],							// all js that should be linted
 	jsUglifyFilesSrc :[],							// all js files that should not be concatinated
 	jsConcactFilesSrc : [],							// all js files that should be concatinated
 	jsSrcFileList : [],								// JS files to include
@@ -58,18 +65,33 @@ var target = {
 gulp.task('sass', function() {
 	gulp.src(['src/scss/kickoff.scss'])
 		.pipe(sass({
-			style: 'compressed',
-			lineNumbers: true
+			style: 'expanded',
+			lineNumbers: false
 		}))
 		.pipe(rename(target.cssDistFile))
 		.pipe(gulp.dest(target.cssDirDist));
 });
 
 
+
+
+
+
+
+
+
 /*******************************************************************************
 4. JS TASKS
 *******************************************************************************/
 // lint custom js
+gulp.task('js-lint', function() {
+    gulp.src(target.jsLintFilesSrc)                     // get the files
+        .pipe(jshint())                                 // lint the files
+        .pipe(jshint.reporter(stylish))                 // present the results in a beautiful way
+});
+
+
+
 
 // minify all js files that should not be concatinated
 
